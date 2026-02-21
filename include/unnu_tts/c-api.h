ebb7f65bafefabddb57acdf131832f2938dcfdbd
Sheldon Robinson <sheldon.robinson@gmail.com>
2026-02-21 16:06:18 -0500

    Fix api

--- include/unnu_tts/c-api.h
+++ include/unnu_tts/c-api.h
@@ -33,7 +33,7 @@ UNNU_TTS_API void ut_terminate();
 
 UNNU_TTS_API void ut_add_speaker(const char* model_path, int32_t voice_id, int32_t speaker_id, const char* name); // sid speaker id, vid voice id
 
-UNNU_TTS_API void ut_rm_speaker(const char* name); 
+UNNU_TTS_API void ut_rm_speaker(int32_t speaker_id);
 
 UNNU_TTS_API int32_t ut_get_speaker_id(const char* name); 
 
